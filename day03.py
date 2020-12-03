ca18b00a375f9d7ef46be6b4cbeb2f6e18b1e9ce
John McCrary <johnmccrarywork@gmail.com>
2020-12-03 10:15:29 -0600

    Remove description

--- day03.py
+++ day03.py
@@ -1,24 +1,5 @@
 #!/usr/bin/env python3
 
-''' 
-Part 1
-------
-
-You start on the open square (.) in the top-left corner and need to reach 
-the bottom (below the bottom-most row on your map).
-
-The toboggan can only follow a few specific slopes (you opted for a cheaper 
-model that prefers rational numbers); start by counting all the trees you 
-would encounter for the slope right 3, down 1:
-
-From your starting position at the top-left, check the position that is 
-right 3 and down 1. Then, check the position that is right 3 and down 1 
-from there, and so on until you go past the bottom of the map.
-
-Starting at the top-left corner of your map and following a slope of right 
-3 and down 1, how many trees would you encounter?
-'''
-
 from operator import mul 
 from functools import reduce
 
